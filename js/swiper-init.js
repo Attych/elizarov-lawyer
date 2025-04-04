7be08779655206ebdcab3cb006d0f33e48753185
Anton Elizarov <attych@gmail.com>
2025-04-04 17:45:26 +0300

    главный блок

---
+++ js/swiper-init.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1200: { slidesPerView: 4 },
+      992: { slidesPerView: 3 },
+      768: { slidesPerView: 2 },
+      576: { slidesPerView: 1 }
+    }
+  });
+  
